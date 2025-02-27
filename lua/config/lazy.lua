82ce69d68563d11e3d35fc16dd7d5c1a8a2e9ae2
mzy <985401575@qq.com>
2025-02-27 16:55:48 +0800

    fix

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
 
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.ai.codeium" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
