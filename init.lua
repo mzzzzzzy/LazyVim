bdba182453e11b5de254375e06fbba3809538cc3
mzy <mengz1534@gmail.com>
2025-05-24 15:50:16 +0800

    add vscode

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+-- 加载 LazyVim 核心配置
 require("config.lazy")
+
+if vim.g.vscode then
+  -- do something
+end
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "plugins" },
+    { import = "lazyvim.plugins.extras.vscode" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
